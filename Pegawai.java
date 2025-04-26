f21550e92ee184e9fc641680e5fb892cc9bd820b
Muhammad Syauqi <168622901+yukaiii19@users.noreply.github.com>
2025-04-26 21:45:26 +0700

    Add files via upload

---
+++ Pegawai.java
@@ -0,0 +1,16 @@
+public class Pegawai {
+    protected String nama;
+    protected int gajiPokok = 5000000;
+
+    public Pegawai(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void tampilData() {
+        System.out.println("Nama : " + this.nama + ", Gaji Pokok : " + this.gajiPokok);
+    }
+}
